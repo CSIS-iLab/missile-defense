13679db0644a75e4d66be28e391bfc606fb6bf10
Jacque Schrag <jnschrag@gmail.com>
2016-10-14 10:37:11 -0400

    Remove feature image from country custom taxonomy

--- wp-content/themes/missilecsis/js-custom-taxonomy-meta.php
+++ wp-content/themes/missilecsis/js-custom-taxonomy-meta.php
@@ -5,9 +5,11 @@
 
 // REGISTER SCRIPTS
 function __js_term_meta_scripts()  {
-
+    //
+    wp_enqueue_media();
     // add theme scripts
     wp_enqueue_script( 'js-custom-taxonomy', get_template_directory_uri() . '/js/js-custom-taxonomy-meta.js', array( 'jquery' ), '', true );
+
 
 }
 add_action( 'admin_enqueue_scripts', '__js_term_meta_scripts' );
@@ -43,7 +45,9 @@ function ___add_form_field_countries_feature_image() { ?>
     <?php wp_nonce_field( basename( __FILE__ ), 'countries_feature_image_nonce' ); ?>
     <div class="form-field countries-feature-image-wrap">
         <label for="countries-feature-image"><?php _e( 'Feature Image', 'text_domain' ); ?></label>
-        <input type="text" name="countries_feature_image" id="countries-feature-image" value="" class="countries-feature-image-field" />
+        <input type="hidden" name="countries_feature_image" id="countries-feature-image" value="" class="countries-feature-image-field" />
+        <div class='countries_feature_image_container'></div>
+        <input type="button" id="meta-image-button" class="button choose-meta-image-button" value="<?php _e( 'Choose or Upload an Image', 'text_domain' )?>" />
     </div>
 <?php }
 
@@ -63,8 +67,16 @@ function ___edit_form_field_countries_feature_image( $term ) {
         <th scope="row"><label for="countries-feature-image"><?php _e( 'Feature Image', 'text_domain' ); ?></label></th>
         <td>
             <?php wp_nonce_field( basename( __FILE__ ), 'countries_feature_image_nonce' ); ?>
-            <input type="text" name="countries_feature_image" id="countries-feature-image" value="<?php echo esc_attr( $value ); ?>" class="countries-feature-image-field"  />
-            <input type="button" id="meta-image-button" class="button" value="<?php _e( 'Choose or Upload an Image', 'text_domain' )?>" />
+            <input type="hidden" name="countries_feature_image" id="countries-feature-image" value="<?php echo esc_attr( $value ); ?>" class="countries-feature-image-field"  />
+            <div class='countries_feature_image_container'>
+            <?php
+                if($value) {
+                    echo "<img src='".$value."' style='width:200px;height:auto;' class='choose-meta-image-button' title='Change Image' /><br />";
+                    echo '<input type="button" id="remove-meta-image-button" class="button" value="Remove Image" />';
+                }
+            ?>
+            </div>
+            <input type="button" id="meta-image-button" class="button choose-meta-image-button" value="<?php _e( 'Choose or Upload an Image', 'text_domain' )?>" />
         </td>
     </tr>
 
@@ -114,10 +126,12 @@ function ___manage_term_custom_column( $out, $column, $term_id ) {
 
         $value  = ___get_countries_feature_image( $term_id );
 
-        if ( ! $value )
-            $value = '';
-
-        $out = sprintf( '<span class="countries-feature-image-block" style="" >%s</div>', esc_attr( $value ) );
+        if ( ! $value ) {
+            $out = "";
+        }
+        else {
+            $out = sprintf( '<img class="countries-feature-image-block" style="width:100%%;height:auto;" src="%s" />', esc_attr( $value ) );
+        }
     }
 
     return $out;
