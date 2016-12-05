140edc886822a0eeee3b03661103c0c241f3ae95
Jacque Schrag <jnschrag@gmail.com>
2016-12-05 13:33:54 -0500

    Install Hero Menu plugin

---
+++ wp-content/plugins/hero-menu/templates/slider-single.php
@@ -0,0 +1,17 @@
+<div id='hero-menu-container'>
+	<div class='feature-background' style='
+		background-image:url("<?php echo $feat_image; ?>");
+		background-position-x:<?php echo $feat_bgpos_x; ?>;
+		background-position-y:<?php echo $feat_bgpos_y; ?>;
+	'>
+		<div class='overlay'>
+			<div class='featuredItem-container'>
+				<div class='featuredItem'>
+					<div class='description'><?php echo $feat_description; ?></div>
+					<div class='title'><a href='<?php echo $feat_link; ?>'><?php echo $feat_title; ?></a></div>
+					<a href='<?php echo $feat_link; ?>' class='seeMore'><?php echo __($feat_cta, 'heroMenu'); ?></a>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
--- wp-content/themes/amti/header.php
+++ wp-content/themes/amti/header.php
@@ -59,8 +59,4 @@
 
 	<div id="content" class="site-content">
 
-	<?php if ( has_nav_menu( 'home-page-slider' ) && is_front_page() ) : ?>
-		<div id="homepage-slider-container">
-			<?php transparency_slider(); ?>
-		</div><!-- #homepage-slider-container -->
-	<?php endif; ?>
+	<?php putHeroMenu(); ?>
