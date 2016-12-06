9bc1599f24339e85b3dd7a35a5a02c71d5b06cb7
Jacque Schrag <jnschrag@gmail.com>
2016-12-06 12:55:09 -0500

    Add search icon to navigation bar

--- wp-content/themes/amti/functions.php
+++ wp-content/themes/amti/functions.php
@@ -283,6 +283,23 @@ array(
 /*-----------------------------------------------------------------------------------*/
 require_once('wp_bootstrap_navwalker.php');
 
+/*-----------------------------------------------------------------------------------*/
+/* Add Search Bar to Menu
+/*-----------------------------------------------------------------------------------*/
+add_filter( 'wp_nav_menu_items','add_search_box', 10, 2 );
+function add_search_box( $items, $args ) {
+	if($args->theme_location == 'primary') {
+	    $search = '<li class="search">';
+	    $search .= '<form method="get" id="searchform" action="/"><div class="input-group">';
+	    $search .= '<label class="screen-reader-text" for="navSearchInput">Search for:</label>';
+	    $search .= '<input type="text" class="form-control" name="s" id="navSearchInput" placeholder="Search" />';
+	    $search .= '<label for="navSearchInput" id="navSearchLabel"><i class="fa fa-search" aria-hidden="true"></i></label>';
+	    $search .= '</div></form>';
+	    $search .= '</li>';
+	    return $items.$search;
+	}
+	return $items;
+}
 
 // Remove comments from media attachments, specifically the comments on the JetPack Carousel Slides
 function filter_media_comment_status( $open, $post_id ) {
