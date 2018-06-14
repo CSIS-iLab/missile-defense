50a41efa6d33c6999ad823832e77e417fb1990d0
Jacque Schrag <jnschrag@gmail.com>
2018-06-14 09:49:04 -0400

    Update footer SCSS to fix conflict.

--- wp-content/themes/missile-defense/footer.php
+++ wp-content/themes/missile-defense/footer.php
@@ -38,11 +38,7 @@
 			</p>
 		</div>
 		<div class="col-sm-6 text-right">
-<<<<<<< HEAD
-<p>&copy; <?php echo date("Y"); ?> by the Center for Strategic and International Studies. <br>All rights reserved. | <a href="https://www.csis.org/privacy-policy" target="_blank" rel="nofollow">Privacy Policy</a></p>
-=======
-			<p>&copy; <?php echo date("Y"); ?> by the Center for Strategic and International Studies. <br>All rights reserved. | <a href="https://www.csis.org/privacy-policy" target="_blank">Privacy Policy</a></p>
->>>>>>> redesign
+			<p>&copy; <?php echo date("Y"); ?> by the Center for Strategic and International Studies. <br>All rights reserved. | <a href="https://www.csis.org/privacy-policy" target="_blank" rel="nofollow">Privacy Policy</a></p>
 		</div>
 	</div>
 	</div>
