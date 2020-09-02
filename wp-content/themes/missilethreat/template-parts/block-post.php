8f0aa25a79480deea68b6011a55294b897022da4
Lindsay Urchyk <urchykli@gmail.com>
2020-09-02 15:14:18 -0400

    Post block content

--- wp-content/themes/missilethreat/template-parts/block-post.php
+++ wp-content/themes/missilethreat/template-parts/block-post.php
@@ -15,16 +15,23 @@
 
 <article <?php post_class('post-block post-block--post'); ?> id="post-<?php the_ID(); ?>">
 
-	<?php
-	missilethreat_posted_on();
-?>
-<p>Hi</p>
 <?php
+
+	foreach (get_the_category() as $category) {
+		if ( $category->name !== 'Featured' ) {
+				echo '<a href="' . get_category_link($category->term_id) . '">' .$category->name . '</a> '; //Markup as you see fit
+		}
+	}
+
 	the_title( '<h3 class="post-block__title"><a href="' . esc_url( get_permalink() ) . '">', '</a></h3>' );
 
-	the_excerpt();
+	missilethreat_posted_on();
 
 	missilethreat_authors();
-	?>
+	
+	the_excerpt();
+
+	the_post_thumbnail(array(200, 160));
+?>
 
 </article><!-- .post -->
