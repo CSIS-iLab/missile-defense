0a0fc9fded9c3e4a0390fe1d6ebd2c8979aa6edd
Lindsay Urchyk <urchykli@gmail.com>
2020-09-17 13:19:14 -0400

    Start scafolding for tier 1 pages

--- wp-content/themes/missilethreat/archive.php
+++ wp-content/themes/missilethreat/archive.php
@@ -1,11 +1,40 @@
 
 <?php
-/* 
-Template Name: Archive Page Custom
+/**
+ *Template Name: Archive Page Custom
+ *
+ * @package CSIS iLab
+ * @subpackage @package MissileThreat
+ * @since 1.0.0
+**/
 
-@subpackage MissileThreat
-@since 1.0.0
-*/
+get_header();
 ?>
 
+<main id="site-content" role="main">
 test
+<header class="tier-1__header">
+
+Heeeadeeeeeerrrrrrrrrrrrrr
+
+</header>
+
+<div class='tier-1'>
+
+  <section class="tier-1__content">
+    
+    sup I'm the content
+    
+  </section>
+  
+  
+  
+  <aside class="tier-1__sidebar">
+    
+    Sidebar ish goes here
+  </aside>
+</div>
+
+</main>
+
+<?php get_footer(); ?>
