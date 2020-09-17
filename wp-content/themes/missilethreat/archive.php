fa93ad02be4e81f2a15bd4ebbcc70f74df133f29
Lindsay Urchyk <urchykli@gmail.com>
2020-09-17 14:38:22 -0400

    Create filter.php

--- wp-content/themes/missilethreat/archive.php
+++ wp-content/themes/missilethreat/archive.php
@@ -12,7 +12,7 @@ get_header();
 ?>
 
 <main id="site-content" role="main">
-test
+
 <header class="tier-1__header">
 
 Heeeadeeeeeerrrrrrrrrrrrrr
@@ -23,7 +23,15 @@ Heeeadeeeeeerrrrrrrrrrrrrr
 
   <section class="tier-1__content">
     
-    sup I'm the content
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras id sem tellus. Nunc eu lacus quis ipsum posuere commodo et non nulla. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Aenean ac commodo ligula. Nam scelerisque ante quis magna venenatis, ac pellentesque leo gravida. Quisque vulputate purus ac odio tincidunt sollicitudin id non elit. Aenean sagittis molestie nunc, vitae euismod justo euismod imperdiet. Curabitur ultricies felis nec pellentesque fermentum. Integer mollis varius tempus. Sed iaculis eget odio id tempus. Vivamus placerat, justo nec tempor commodo, nisl justo posuere nisi, eu cursus sem ex vitae mauris. Praesent fringilla magna quis ipsum aliquet, sed feugiat erat dictum.
+
+Integer nec rutrum velit, at laoreet dui. Aenean vel mattis nulla, in facilisis metus. Sed eget enim bibendum, viverra urna sit amet, vestibulum augue. Proin aliquam malesuada risus nec sollicitudin. Phasellus vitae arcu ac felis facilisis placerat eu in odio. Nulla tortor tellus, pulvinar eu mauris eu, ullamcorper vestibulum dui. Aenean congue tempus convallis. Mauris odio dui, sodales et ex at, posuere dignissim mauris. Sed a interdum lorem. Mauris mollis facilisis nisi eu faucibus. Nulla est tortor, vulputate nec aliquam sit amet, facilisis hendrerit massa. Quisque ultrices laoreet urna, ut sagittis augue sagittis ut.
+
+Sed bibendum faucibus lectus finibus iaculis. Vestibulum tempor mi nec ligula lobortis ultricies. Ut id felis ut risus faucibus bibendum. Ut ut elit egestas, venenatis justo sodales, mollis justo. Proin at sagittis ex. Sed viverra bibendum arcu eu rutrum. Quisque vitae interdum ipsum. Integer blandit nulla non neque consequat dignissim. Sed vitae hendrerit ante.
+
+Mauris quis massa mauris. Ut tincidunt nisl turpis, non egestas tortor varius et. Fusce sagittis ex sagittis justo aliquam pulvinar. Morbi venenatis molestie arcu, eget porttitor odio hendrerit et. Curabitur rutrum sagittis sapien, at mollis velit rhoncus nec. Suspendisse potenti. Ut enim ante, pellentesque quis velit mollis, feugiat blandit urna. Donec venenatis tempus orci vitae tristique.
+
+Nam efficitur consequat neque, tincidunt rutrum urna aliquam vel. Nunc in cursus lorem, et rhoncus nunc. Ut viverra elit orci, ac blandit velit mattis eget. In elementum imperdiet turpis, eu vehicula turpis. Sed placerat lobortis dui a dictum. Aenean et nisl dignissim mauris pulvinar consequat. Duis tincidunt diam ac ipsum pulvinar accumsan. Donec mattis lobortis ligula, tempor viverra ex pretium non. Mauris molestie tortor et magna tincidunt cursus.
     
   </section>
   
