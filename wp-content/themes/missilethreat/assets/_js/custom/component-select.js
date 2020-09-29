d51e2504bc5785a2876f15b8bd11646e44d0b951
Lindsay Urchyk <urchykli@gmail.com>
2020-09-29 13:29:23 -0400

    Refactor missile and system dropdowns

--- wp-content/themes/missilethreat/assets/_js/custom/component-select.js
+++ wp-content/themes/missilethreat/assets/_js/custom/component-select.js
@@ -1,52 +1,24 @@
-let systemSubmit = document.querySelector('#system-select-go')
-let systemSelect = document.querySelector('#system-select')
-let missileSubmit = document.querySelector('#missile-select-go')
-let missileSelect = document.querySelector('#missile-select')
+document.addEventListener('DOMContentLoaded', function () {
+  let itemSubmit = document.querySelector('#item-select-go')
+  let itemSelect = document.querySelector('#item-select')
+  let missileSubmit = document.querySelector('#missile-select-go')
+  let missileSelect = document.querySelector('#missile-select')
 
-if (systemSubmit) {
-  systemSubmit.addEventListener(
-    'submit',
-    function () {
-      let systemSelection = document.querySelector('#system-select').value
+  if (itemSubmit) {
+    itemSubmit.addEventListener('click', function () {
+      window.location.href = itemSelect.value
+    })
+  }
 
-      window.location.href = systemSelection
-    },
-    false
-  )
-}
-
-if (systemSelect) {
-  systemSelect.addEventListener(
-    'keypress',
-    function (e) {
-      if (e.key === 'Enter') {
-        window.location.href = e.target.value
-      }
-    },
-    false
-  )
-}
-
-if (missileSubmit) {
-  missileSubmit.addEventListener(
-    'submit',
-    function () {
-      let missileSelection = document.querySelector('#missile-select').value
-
-      window.location.href = missileSelection
-    },
-    false
-  )
-}
-
-if (missileSelect) {
-  missileSelect.addEventListener(
-    'keypress',
-    function (e) {
-      if (e.key === 'Enter') {
-        window.location.href = e.target.value
-      }
-    },
-    false
-  )
-}
+  if (itemSelect) {
+    itemSelect.addEventListener(
+      'keypress',
+      function (e) {
+        if (e.key === 'Enter') {
+          window.location.href = e.target.value
+        }
+      },
+      false
+    )
+  }
+})
--- wp-content/themes/missilethreat/template-parts/sidebar.php
+++ wp-content/themes/missilethreat/template-parts/sidebar.php
@@ -7,54 +7,39 @@
  * @since 1.0.0
  */
 
+function get_archive_post_type() {
+	return is_archive() ? get_queried_object()->name : false;
+}
 
-if( is_post_type_archive( 'defsys' ) ) {
-$query = new WP_Query( array(
-		'post_type' => 'defsys',
-		'post_status' => 'publish',
-		'orderby' => 'name',
-		'order' => 'ASC',
-		'posts_per_page' => -1
-) )
-	?>
-
-	<label for="system-select" class="component-select__label">SEARCH</label>
-	<div class=component-select>
-	<select name="componentSelect" id="system-select" class="component-select__dropdown"><option value="">Choose a component...</option>
-
-	<?php
-	while( $query->have_posts() ) {
-		$query->the_post();
-		echo '<option value="' . esc_url( get_permalink() ) . '" class="component-select__option">' . get_the_title() . '</option>';
-	}
-
-	echo '</select><button type="submit" form="system-select" class="btn btn--dark component-select__submit" id="system-select-go">Go</button></div>';
-
-	wp_reset_postdata();
-
-} elseif ( is_post_type_archive( 'missile' ) ) {
+if( get_archive_post_type() ) {
 	$query = new WP_Query( array(
-		'post_type' => 'missile',
-		'post_status' => 'publish',
-		'orderby' => 'name',
-		'order' => 'ASC',
-		'posts_per_page' => -1
-) )
-	?>
+			'post_type' => get_archive_post_type(),
+			'post_status' => 'publish',
+			'orderby' => 'name',
+			'order' => 'ASC',
+			'posts_per_page' => -1
+	) );
 
-	<label for="missile-select" class="component-select__label">SEARCH</label>
-	<div class=component-select>
-		<select name="missileSelect" id="missile-select" class="component-select__dropdown"><option value="">Choose a missile...</option>
-
-	<?php
-	while( $query->have_posts() ) {
-		$query->the_post();
-		echo '<option value="' . esc_url( get_permalink() ) . '" class="component-select__option">' . get_the_title() . '</option>';
+	if( get_archive_post_type() === 'defsys') {
+		$archive_type = 'component';
+	} elseif( get_archive_post_type() === 'missile' ) {
+		$archive_type = 'missile';
 	}
-
-	echo '</select><button type="submit" form="missile-select" class="component-select__submit" id="missile-select-go">Go</button></div>';
-
-	wp_reset_postdata();
+		?>
+	
+		<label for="item-select" class="component-select__label">Search</label>
+		<div class="component-select">
+		<select name="componentSelect" id="item-select" class="component-select__dropdown"><option value="">Choose a <?php echo $archive_type; ?>...</option>
+	
+		<?php
+		while( $query->have_posts() ) {
+			$query->the_post();
+			echo '<option value="' . esc_url( get_permalink() ) . '" class="component-select__option">' . get_the_title() . '</option>';
+		}
+	
+		echo '</select><button type="submit" form="item-select" class="btn btn--dark component-select__submit" id="item-select-go">Go</button></div>';
+	
+		wp_reset_postdata();
 
 } else {
 	dynamic_sidebar( 'sidebar' );
