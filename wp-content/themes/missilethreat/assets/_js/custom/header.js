fde8d89cb9dac7cc070f4079dec961ad9ed6af0b
Lindsay Urchyk <urchykli@gmail.com>
2020-10-16 16:01:41 -0400

    Add transition to header elements

--- wp-content/themes/missilethreat/assets/_js/custom/header.js
+++ wp-content/themes/missilethreat/assets/_js/custom/header.js
@@ -6,14 +6,13 @@ const searchIcon = document.querySelector('.header__search-icon')
 const searchClose = document.querySelector('.header__search-close')
 const searchForm = document.querySelector('.header__search')
 const content = document.querySelector('#site-content')
+const header = document.querySelector('.header')
 
 hamburger.addEventListener('click', () => {
   menu.classList.add('visible')
   navClose.classList.add('visible')
   hamburger.classList.add('invisible')
 })
-
-navClose.addEventListener('click', closeNav)
 
 searchIcon.addEventListener('click', () => {
   searchForm.classList.add('visible')
@@ -24,17 +23,6 @@ searchIcon.addEventListener('click', () => {
   navClose.classList.remove('visible')
 })
 
-searchClose.addEventListener('click', () => {
-  searchForm.classList.remove('visible')
-  searchIcon.classList.remove('invisible')
-  logo.classList.remove('invisible')
-  hamburger.classList.remove('invisible')
-})
-
-searchClose.addEventListener('click', closeSearch)
-
-content.addEventListener('click', closeNav)
-content.addEventListener('click', closeSearch)
 
 function closeNav() {
   menu.classList.remove('visible')
@@ -43,10 +31,26 @@ function closeNav() {
 }
 
 function closeSearch() {
-  menu.classList.remove('visible')
-  navClose.classList.remove('visible')
   searchForm.classList.remove('visible')
   searchIcon.classList.remove('invisible')
   logo.classList.remove('invisible')
   hamburger.classList.remove('invisible')
+  menu.classList.remove('visible')
+  navClose.classList.remove('visible')
 }
+
+function headerColor() {
+  if (window.scrollY >= 72) {
+    header.classList.add('full-color')
+  } else {
+    header.classList.remove('full-color')
+  }
+}
+
+navClose.addEventListener('click', closeNav)
+content.addEventListener('click', closeNav)
+
+searchClose.addEventListener('click', closeSearch)
+content.addEventListener('click', closeSearch)
+
+window.addEventListener('scroll', headerColor)
