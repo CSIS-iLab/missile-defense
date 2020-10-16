e299a3dd8271db30ef51dae9ab97becfa49f899e
MrBachChoi <55605733+MrBachChoi@users.noreply.github.com>
2020-10-16 12:03:06 -0400

    Initial progress on footer styling

--- wp-content/themes/missilethreat/footer.php
+++ wp-content/themes/missilethreat/footer.php
@@ -12,13 +12,25 @@
  */
 
 ?>
+	<style>
+	@import 'assets/_scss/layout/_footer';
+	</style>
+		<div class="footer-container">
 			<footer id="site-footer" class="footer" role="contentinfo">
 				<a href="https://www.csis.org" class="footer__logo"><?php include( get_template_directory() . '/assets/static/csis-logo.svg'); ?></a>
 
-				<?php dynamic_sidebar( 'footer-1' ); ?>
+				<div class="description-container">
+					<p class="description">
+					Missile Threat brings together a wide range of information and analyses relating to the proliferation of cruise and ballistic missiles around the world and the air and missile defense systems designed to defeat them. Missile Threat is a product of the <span class="mdp">Missile Defense Project</span> at the Center for Strategic and International Studies.
+					</p>
+				</div>
 
 				<div class="footer__contact">
 					<address class="footer__address">
+					<div class="social-icons">
+					<a href="/" class="facebook-icon"><?php echo missilethreat_get_svg( 'facebook' ); ?></a>
+					<a href="/" class="twitter-icon"><?php echo missilethreat_get_svg( 'twitter' ); ?></a>
+					</div>
 						1616 Rhode Island Ave NW<br />
 						Washington, DC 20036
 					</address>
@@ -28,6 +40,15 @@
 					<?php dynamic_sidebar( 'social-share' ); ?>
 				</div>
 
+				<div class="news_letter">
+					<h4 class="news-title">Newsletter</h4>
+					<p class="news-body">"PLACEHOLDER" The CSIS Missile Defense Project’s monthly newsletter has info on the project’s latest publications, events, and missile defense news.</p>
+					<button class="btn btn--light text--semibold">SUBSCRIBE</button>
+				</div>
+
+				<div class="nav_side">
+					<?php dynamic_sidebar( 'footer-1' ); ?>
+				</div>
 
 				<p class="footer__copyright">Copyright &copy;
 					<?php
@@ -36,7 +57,7 @@
 						_x( 'Y', 'copyright date format', 'missilethreat' )
 					);
 					?>
-					Center for Strategic and International Studies.<br />All rights reserved. <a href="https://www.csis.org/privacy-policy">Privacy Policy</a>
+					Center for Strategic and International Studies.<br />All rights reserved. | <a class="pp" href="https://www.csis.org/privacy-policy">Privacy Policy</a>
 				</p><!-- .footer-copyright -->
 
 			</footer><!-- #site-footer -->
