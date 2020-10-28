8e95a58d784e9dbbfebd64421fe123b49c1f5040
MrBachChoi <55605733+MrBachChoi@users.noreply.github.com>
2020-10-28 16:44:32 -0400

    Framework for footer. Closes #83

--- wp-content/themes/missilethreat/footer.php
+++ wp-content/themes/missilethreat/footer.php
@@ -17,39 +17,57 @@
 	</style>
 		<div class="footer-container">
 			<footer id="site-footer" class="footer" role="contentinfo">
-				<a href="https://www.csis.org" class="footer__logo"><?php include( get_template_directory() . '/assets/static/csis-logo.svg'); ?></a>
+			<div class="footer__logo">	
+			<p><a href="https://www.csis.org"><?php include( get_template_directory() . '/assets/static/csis-logo.svg'); ?></a></p>
+			</div>
 
+			<div class="grid-container">
 				<div class="description-container">
 					<p class="description">
 					Missile Threat brings together a wide range of information and analyses relating to the proliferation of cruise and ballistic missiles around the world and the air and missile defense systems designed to defeat them. Missile Threat is a product of the <span class="mdp">Missile Defense Project</span> at the Center for Strategic and International Studies.
 					</p>
-				</div>
+				
 
-				<div class="footer__contact">
-					<address class="footer__address">
-					<div class="social-icons">
-					<a href="/" class="facebook-icon"><?php echo missilethreat_get_svg( 'facebook' ); ?></a>
-					<a href="/" class="twitter-icon"><?php echo missilethreat_get_svg( 'twitter' ); ?></a>
-					</div>
-						1616 Rhode Island Ave NW<br />
-						Washington, DC 20036
-					</address>
+					<div class="footer__contact">
+						<address class="footer__address">
+						<div class="flex-container">
+							<div class="social-icons">
+								<span class="social-icon-fb"><a href="/" class="facebook_icon"><?php echo missilethreat_get_svg( 'facebook' ); ?></a></span>
+								<span class="social-icon-tw"><a href="/" class="twitter_icon"><?php echo missilethreat_get_svg( 'twitter' ); ?></a></span>
+							</div>
 
-					<p class="footer__phone">(202) 887-0200</p>
+							<div class="contact-address">
+								<span class="address">
+								1616 Rhode Island Ave NW<br />
+								Washington, DC 20036
+								</span>
+							</div>
+						</div>
+						</address>
+
+					<!-- <p class="footer__phone">(202) 887-0200</p> -->
 
 					<?php dynamic_sidebar( 'social-share' ); ?>
+					</div>
 				</div>
 
-				<div class="news_letter">
-					<h4 class="news-title">Newsletter</h4>
-					<p class="news-body">"PLACEHOLDER" The CSIS Missile Defense Project’s monthly newsletter has info on the project’s latest publications, events, and missile defense news.</p>
-					<button class="btn btn--light text--semibold">SUBSCRIBE</button>
+				<div class="news-nav">
+					<div class="news_letter">
+						<h4 class="news-title">Newsletter</h4>
+						<div class="news-body">
+						<?php dynamic_sidebar( 'newsletter' ); ?>
+						</div>
+						<div class="footer-button">
+						<button class="btn btn--light text--semibold">SUBSCRIBE</button>
+						</div>
+					</div>
+
+					<div class="nav_side">
+						<?php dynamic_sidebar( 'footer-1' ); ?>
+					</div>
 				</div>
 
-				<div class="nav_side">
-					<?php dynamic_sidebar( 'footer-1' ); ?>
-				</div>
-
+			</div>
 				<p class="footer__copyright">Copyright &copy;
 					<?php
 					echo date_i18n(
@@ -59,7 +77,7 @@
 					?>
 					Center for Strategic and International Studies.<br />All rights reserved. | <a class="pp" href="https://www.csis.org/privacy-policy">Privacy Policy</a>
 				</p><!-- .footer-copyright -->
-
+			</div>
 			</footer><!-- #site-footer -->
 
 		</div><!-- .container -->
