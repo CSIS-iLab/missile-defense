5c42d237d0de50161f151bfb057ab4200713bf8e
Lindsay Urchyk <urchykli@gmail.com>
2020-10-28 17:01:35 -0400

    Homepage (#120)
    * Homepage mobile and small layouts
    
    * Homepage medium layout
    
    * Homepage styling for large breakpoint
    
    * Homepage styling for xl and xxl breakpoints
    
    * Homepage twitter widget
    
    * Events data added to homepage
    
    * Event and link hover styling
    
    * Link hover classe added to homepage links
    
    * Post block link structure and classes
    
    * Post block categories uppercase
    
    * Function to link thumbnails to posts
    
    * Thumbnails as links function moved to post blocks
    
    * Uppercase month in events section
    
    * Add position to homepage cards to fix z index
    
    * Set max-width on homepage cards at medium breakpoint
    
    * Minor style tweaks to ongoing projects section on homepage
    
    * Spacing between ongoing projects title and description
    
    * Decrease space between news section header and first article
    
    * Move negative margin from featured post to hero
    
    * Add borders to recent column 2
    
    * Add border to events
    
    * Align hero elements to flex end
    
    * Hero top padding on large breakpoint
    
    * Hero background color
    
    * Refactor events section
    
    * Remove unneeded code
    
    * Combine like elements on homepage style

--- wp-content/themes/missilethreat/template-parts/block-post-featured.php
+++ wp-content/themes/missilethreat/template-parts/block-post-featured.php
@@ -15,16 +15,18 @@
 
 <article <?php post_class('featured-post'); ?> id="post-<?php the_ID(); ?>">
 
-	<?php
-		the_post_thumbnail(array(400, 304));
-?>
+	<?php if ( has_post_thumbnail()) : ?>
+    <a href="<?php the_permalink(); ?>" alt="<?php the_title_attribute(); ?>" class="thumbnail-link">
+        <?php the_post_thumbnail(array(400, 304)); ?>
+    </a>
+	<?php endif; ?>
 <div class="featured-post__category">
 <?php
 	missilethreat_display_categories();
 	?>
 </div>
 <?php
-	the_title( '<h2 class="featured-post__title text--semibold"><a href="' . esc_url( get_permalink() ) . '">', '</a></h2>' );
+	the_title( '<h2 class="featured-post__title text--semibold"><a href="' . esc_url( get_permalink() ) . '" class="post-title--hover">', '</a></h2>' );
 
 	missilethreat_posted_on();
 
--- wp-content/themes/missilethreat/template-parts/block-project.php
+++ wp-content/themes/missilethreat/template-parts/block-project.php
@@ -18,9 +18,9 @@
 <a href=" <?php the_permalink(); ?>"><?php the_post_thumbnail('medium', array('class' => 'project-block__thumbnail')); ?></a>	
 <div class="project-block--details">
 	<?php
-		the_title( '<h2 class="project-block__title text--semibold"><a href="' . esc_url( get_permalink() ) . '">', '</a></h2>' );
+		the_title( '<h2 class="project-block__title text--semibold"><a href="' . esc_url( get_permalink() ) . '" class="post-title--hover">', '</a></h2>' );
 		?>
-		<div class="project-block__updated">Last Updated <?php missilethreat_last_updated(); ?></div>
+		<div class="project-block__updated">Updated <?php missilethreat_last_updated(); ?></div>
 </div>
 
 </article><!-- .post -->
