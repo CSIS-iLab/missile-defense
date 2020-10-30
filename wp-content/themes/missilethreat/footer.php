39f98bc2b8a23314061e395360cd9946ff7e7d68
MrBachChoi <55605733+MrBachChoi@users.noreply.github.com>
2020-10-30 14:50:13 -0400

    Adjusted color and names of classes for review.

--- wp-content/themes/missilethreat/footer.php
+++ wp-content/themes/missilethreat/footer.php
@@ -18,64 +18,52 @@
 		<div class="footer-container">
 			<footer id="site-footer" class="footer" role="contentinfo">
 			<div class="footer__logo">	
-			<p><a href="https://www.csis.org"><?php include( get_template_directory() . '/assets/static/csis-logo.svg'); ?></a></p>
+			<a href="https://www.csis.org"><?php include( get_template_directory() . '/assets/static/csis-logo.svg'); ?></a>
 			</div>
 
-			<div class="grid-container">
-				<div class="description-container">
-					<p class="description">
-					Missile Threat brings together a wide range of information and analyses relating to the proliferation of cruise and ballistic missiles around the world and the air and missile defense systems designed to defeat them. Missile Threat is a product of the <span class="mdp">Missile Defense Project</span> at the Center for Strategic and International Studies.
+			<div class="footer_large_container">
+				<div class="footer_description">
+					<p>
+					Missile Threat brings together a wide range of information and analyses relating to the proliferation of cruise and ballistic missiles around the world and the air and missile defense systems designed to defeat them. Missile Threat is a product of the <span class="footer_mdp">Missile Defense Project</span> at the Center for Strategic and International Studies.
 					</p>
 				
 
 					<div class="footer__contact">
-						<address class="footer__address">
-						<div class="flex-container">
-							<div class="social-icons">
-								<span class="social-icon-fb"><a href="/" class="facebook_icon"><?php echo missilethreat_get_svg( 'facebook' ); ?></a></span>
-								<span class="social-icon-tw"><a href="/" class="twitter_icon"><?php echo missilethreat_get_svg( 'twitter' ); ?></a></span>
-							</div>
-
-							<div class="contact-address">
-								<span class="address">
-								1616 Rhode Island Ave NW<br />
-								Washington, DC 20036
-								</span>
-							</div>
+						<div class="footer_social_icons">
+							<a href="/" class="facebook_icon"><?php echo missilethreat_get_svg( 'facebook' ); ?></a>
+							<a href="/" class="twitter_icon"><?php echo missilethreat_get_svg( 'twitter' ); ?></a>
 						</div>
-						</address>
+
+						<div class="footer_address">
+							1616 Rhode Island Ave NW<br />
+							Washington, DC 20036
+						</div>
 
 					<!-- <p class="footer__phone">(202) 887-0200</p> -->
 
 					<?php dynamic_sidebar( 'social-share' ); ?>
-					</div>
 				</div>
+			</div>
 
-				<div class="news-nav">
-					<div class="news_letter">
-						<h4 class="news-title">Newsletter</h4>
-						<div class="news-body">
+				<div class="placeholder_news_nav">
+					<div class="footer_news_letter">
 						<?php dynamic_sidebar( 'newsletter' ); ?>
-						</div>
-						<div class="footer-button">
-						<button class="btn btn--light text--semibold">SUBSCRIBE</button>
-						</div>
 					</div>
 
-					<div class="nav_side">
+					<div class="footer_nav_side">
 						<?php dynamic_sidebar( 'footer-1' ); ?>
 					</div>
 				</div>
 
 			</div>
-				<p class="footer__copyright">Copyright &copy;
+				<p class="footer__copyright">&copy;
 					<?php
 					echo date_i18n(
 						/* translators: Copyright date format, see https://secure.php.net/date */
 						_x( 'Y', 'copyright date format', 'missilethreat' )
 					);
 					?>
-					Center for Strategic and International Studies. All rights reserved. | <a class="pp" href="https://www.csis.org/privacy-policy">Privacy Policy</a>
+					Center for Strategic and International Studies. All rights reserved. | <a class="footer_pp" href="https://www.csis.org/privacy-policy">Privacy Policy</a>
 				</p><!-- .footer-copyright -->
 			</div>
 			</footer><!-- #site-footer -->
