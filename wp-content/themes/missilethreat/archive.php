26b7e61d6656d57a245d704ec4a650cfc20e4ed2
Lindsay Urchyk <urchykli@gmail.com>
2020-11-02 14:52:04 -0500

    Move page header styles to separate file

--- wp-content/themes/missilethreat/archive.php
+++ wp-content/themes/missilethreat/archive.php
@@ -30,17 +30,17 @@ $page_for_posts = get_option( 'page_for_posts' );
   <?php 
   if ( is_post_type_archive() ) { 
     $feat_image = 'style="background-image:url('.get_archive_thumbnail_src("full").');"';?>
-    <header class="archive__header" <?php echo $feat_image; ?>>
-    <h1 class="archive__title text--semibold"><?php the_archive_title(); ?></h1>
-    <div class="archive__divider"></div>
-    <div class="archive__desc"><?php the_archive_top_content(); ?> </div>
+    <header class="page__header-header" <?php echo $feat_image; ?>>
+    <h1 class="page__header-title text--semibold"><?php the_archive_title(); ?></h1>
+    <div class="page__header-divider"></div>
+    <div class="page__header-desc"><?php the_archive_top_content(); ?> </div>
   
   <?php } elseif ( is_archive() ) { ?>
-    <header class="archive__header archive__header--short" style='background:linear-gradient(180deg, rgba(7, 52, 74, 0) 0%, rgba(6, 43, 61, 0.9) 75.52%), linear-gradient(90deg, #2A5565 0%, #5F7981 100%);'>
+    <header class="page__header page__header--short" style='background:linear-gradient(180deg, rgba(7, 52, 74, 0) 0%, rgba(6, 43, 61, 0.9) 75.52%), linear-gradient(90deg, #2A5565 0%, #5F7981 100%);'>
     <div class="overlay"></div>
-    <h1 class="archive__title text--semibold"><?php the_archive_title(); ?></h1>
-    <div class="archive__divider archive__divider--short"></div>
-    <div class="archive__desc archive__desc--short"><?php the_archive_top_content(); ?> </div>
+    <h1 class="page__header-title text--semibold"><?php the_archive_title(); ?></h1>
+    <div class="page__header-divider page__header-divider--short"></div>
+    <div class="page__header-desc page__header-desc--short"><?php the_archive_top_content(); ?> </div>
     
   <?php } elseif ( $page_for_posts ) { 
     $feat_image = 'style="background-image:url('.get_the_post_thumbnail_url( $page_for_posts ).');"';?>
@@ -48,9 +48,9 @@ $page_for_posts = get_option( 'page_for_posts' );
     <?php
     $post = get_page($page_for_posts);
     setup_postdata($post); ?>
-    <h1 class="archive__title text--semibold"><?php the_title(); ?></h1>
-    <div class="archive__divider"></div>
-    <div class="archive__desc"><?php the_content(); ?> </div>
+    <h1 class="page__header-title text--semibold"><?php the_title(); ?></h1>
+    <div class="page__header-divider"></div>
+    <div class="page__header-desc"><?php the_content(); ?> </div>
 
   <?php }
 
