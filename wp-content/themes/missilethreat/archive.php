9ebf78e41af78f46550a22e2b0d5be616753a77c
Lindsay Urchyk <urchykli@gmail.com>
2020-11-02 14:23:10 -0500

    Style archive labels

--- wp-content/themes/missilethreat/archive.php
+++ wp-content/themes/missilethreat/archive.php
@@ -31,31 +31,28 @@ $page_for_posts = get_option( 'page_for_posts' );
   if ( is_post_type_archive() ) { 
     $feat_image = 'style="background-image:url('.get_archive_thumbnail_src("full").');"';?>
     <header class="archive__header" <?php echo $feat_image; ?>>
+    <h1 class="archive__title text--semibold"><?php the_archive_title(); ?></h1>
+    <div class="archive__divider"></div>
+    <div class="archive__desc"><?php the_archive_top_content(); ?> </div>
   
   <?php } elseif ( is_archive() ) { ?>
-    <header class="archive__header" style='background-image:linear-gradient(180deg, rgba(7, 52, 74, 0) 0%, rgba(6, 43, 61, 0.9) 75.52%), linear-gradient(90deg, #2A5565 0%, #5F7981 100%);'>
-
+    <header class="archive__header archive__header--short" style='background:linear-gradient(180deg, rgba(7, 52, 74, 0) 0%, rgba(6, 43, 61, 0.9) 75.52%), linear-gradient(90deg, #2A5565 0%, #5F7981 100%);'>
+    <div class="overlay"></div>
+    <h1 class="archive__title text--semibold"><?php the_archive_title(); ?></h1>
+    <div class="archive__divider archive__divider--short"></div>
+    <div class="archive__desc archive__desc--short"><?php the_archive_top_content(); ?> </div>
+    
   <?php } elseif ( $page_for_posts ) { 
     $feat_image = 'style="background-image:url('.get_the_post_thumbnail_url( $page_for_posts ).');"';?>
     <header class="archive__header" <?php echo $feat_image; ?>>
+    <?php
+    $post = get_page($page_for_posts);
+    setup_postdata($post); ?>
+    <h1 class="archive__title text--semibold"><?php the_title(); ?></h1>
+    <div class="archive__divider"></div>
+    <div class="archive__desc"><?php the_content(); ?> </div>
 
   <?php }
-
-
-if ( is_archive() ) { ?>
-  <h1 class="archive__title text--semibold"><?php the_archive_title(); ?></h1>
-  <div class="archive__divider"></div>
-  <div class="archive__desc"><?php the_archive_top_content(); ?> </div>
-
-<?php }  
-elseif ( $page_for_posts ) {
-  $post = get_page($page_for_posts);
-  setup_postdata($post); ?>
-  <h1 class="archive__title text--semibold"><?php the_title(); ?></h1>
-  <div class="archive__divider"></div>
-  <div class="archive__desc"><?php the_content(); ?> </div>
-
-<?php }
 
 wp_reset_postdata();
 
