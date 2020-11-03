87421df47c4d375d3d967464a049e9367c08c0cd
Lindsay Urchyk <urchykli@gmail.com>
2020-11-03 10:45:23 -0500

    Search placeholder style

--- wp-content/themes/missilethreat/searchform.php
+++ wp-content/themes/missilethreat/searchform.php
@@ -21,6 +21,6 @@ $aria_label = ! empty( $args['label'] ) ? 'aria-label="' . esc_attr( $args['labe
 ?>
 <form role="search" <?php echo $aria_label; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped -- Escaped above. ?> method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
 	<label class="screen-reader-text" for="<?php echo esc_attr( $unique_id ); ?>">Search for:</label>
-	<input type="search" id="<?php echo esc_attr( $unique_id ); ?>" class="search-field" placeholder="<?php echo esc_attr_x( 'Search &hellip;', 'placeholder', 'missilethreat' ); ?>" value="<?php echo get_search_query(); ?>" name="s" />
+	<input type="search" id="<?php echo esc_attr( $unique_id ); ?>" class="search-field" placeholder="<?php echo esc_attr_x( 'Search&hellip;', 'placeholder', 'missilethreat' ); ?>" value="<?php echo get_search_query(); ?>" name="s" />
 	<button class="search-submit btn btn--light btn--icon" type="submit" aria-label="Submit search" value="<?php echo esc_attr_x( 'Search', 'submit button', 'missilethreat' ); ?>">Search <?php echo missilethreat_get_svg('search') ?></button>
 </form>
