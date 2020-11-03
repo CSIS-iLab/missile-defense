2ea92198de7c6057135a96f12f40ab9f133be5e2
Lindsay Urchyk <urchykli@gmail.com>
2020-11-03 16:35:44 -0500

    Move page header code to entry header template part

--- wp-content/themes/missilethreat/template-parts/entry-header.php
+++ wp-content/themes/missilethreat/template-parts/entry-header.php
@@ -7,28 +7,48 @@
  * @since 1.0.0
  */
 
-$entry_header_classes = '';
+
+$page_for_posts = get_option( 'page_for_posts' );
 ?>
 
-<header class="single__header<?php echo esc_attr( $entry_header_classes ); ?>">
+<?php 
+  if ( is_post_type_archive() ) { 
+    $feat_image = 'style="background-image:url('.get_archive_thumbnail_src( 'missilethreat-fullscreen' ).');"';?>
+		<header class="page__header" <?php echo $feat_image; ?>>
+		<div class="overlay"></div>
+    <h1 class="page__header-title text--semibold"><?php the_archive_title(); ?></h1>
+    <div class="page__header-divider"></div>
+    <div class="page__header-desc"><?php the_archive_top_content(); ?> </div>
+  
+  <?php } elseif ( is_archive() ) { ?>
+    <header class="page__header page__header--short" style='background:linear-gradient(180deg, rgba(7, 52, 74, 0) 0%, rgba(6, 43, 61, 0.9) 75.52%), linear-gradient(90deg, #2A5565 0%, #5F7981 100%);'>
+    <div class="overlay"></div>
+    <h1 class="page__header-title text--semibold"><?php the_archive_title(); ?></h1>
+    <div class="page__header-divider page__header-divider--short"></div>
+    <div class="page__header-desc page__header-desc--short"><?php the_archive_top_content(); ?> </div>
+    
+  <?php } elseif ( is_page() ) { 
+		$feat_image = get_the_post_thumbnail_url( $post->ID ); ?>
+		<header class="page__header"  style="background-image: url('<?php echo $feat_image; ?>')">
+		<div class="overlay"></div>
+    <h1 class="page__header-title text--semibold"><?php the_title(); ?></h1>
+    <div class="page__header-divider"></div>
 
-	<div class="single__header-wrapper">
+  <?php } elseif ( $page_for_posts ) { 
+    $feat_image = 'style="background-image:url('.get_the_post_thumbnail_url( $page_for_posts ).');"';?>
+		<header class="page__header" <?php echo $feat_image; ?>>
+		<div class="overlay"></div>
+    <?php
+    $post = get_page($page_for_posts);
+    setup_postdata($post); ?>
+    <h1 class="page__header-title text--semibold"><?php the_title(); ?></h1>
+    <div class="page__header-divider"></div>
+    <div class="page__header-desc"><?php the_content(); ?> </div>
 
-		<?php
+  <?php }
 
-			missilethreat_posted_on();
+wp_reset_postdata();
 
-			the_title( '<h1 class="single__title">', '</h1>' );
+?>
 
-			if ( has_excerpt() && is_singular() ) {
-				the_excerpt();
-			}
-
-			missilethreat_authors();
-
-			get_template_part( 'template-parts/featured-image' );
-		?>
-
-	</div><!-- .entry-header-inner -->
-
-</header><!-- .entry-header -->
+</header>
