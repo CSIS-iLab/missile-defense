7769608070dcbd46700c4524dddedb3c0d494d2b
Lindsay Urchyk <urchykli@gmail.com>
2020-11-05 17:09:27 -0500

    Add entry-header class to page headers

--- wp-content/themes/missilethreat/template-parts/entry-header.php
+++ wp-content/themes/missilethreat/template-parts/entry-header.php
@@ -14,7 +14,7 @@ $page_for_posts = get_option( 'page_for_posts' );
 <?php 
   if ( is_post_type_archive() ) { 
     $feat_image = 'style="background-image:url('.get_archive_thumbnail_src( 'missilethreat-fullscreen' ).');"';?>
-		<header class="page__header" <?php echo $feat_image; ?>>
+		<header class="page__header entry-header" <?php echo $feat_image; ?>>
 			<div class="overlay"></div>
 			<h1 class="page__header-title text--semibold"><?php the_archive_title(); ?></h1>
 			<div class="page__header-divider"></div>
@@ -23,7 +23,7 @@ $page_for_posts = get_option( 'page_for_posts' );
 	<?php } 
 	
 	elseif ( is_archive() ) { ?>
-    <header class="page__header page__header--short" style='background:linear-gradient(180deg, rgba(7, 52, 74, 0) 0%, rgba(6, 43, 61, 0.9) 75.52%), linear-gradient(90deg, #2A5565 0%, #5F7981 100%);'>
+    <header class="page__header page__header--short entry-header" style='background:linear-gradient(180deg, rgba(7, 52, 74, 0) 0%, rgba(6, 43, 61, 0.9) 75.52%), linear-gradient(90deg, #2A5565 0%, #5F7981 100%);'>
 			<div class="overlay"></div>
 			<h1 class="page__header-title text--semibold"><?php the_archive_title(); ?></h1>
 			<div class="page__header-divider page__header-divider--short"></div>
@@ -33,7 +33,7 @@ $page_for_posts = get_option( 'page_for_posts' );
 	
 	elseif ( is_page() ) { 
 		$feat_image = get_the_post_thumbnail_url( $post->ID ); ?>
-		<header class="page__header"  style="background-image: url('<?php echo $feat_image; ?>')">
+		<header class="page__header entry-header"  style="background-image: url('<?php echo $feat_image; ?>')">
 			<div class="overlay"></div>
 			<h1 class="page__header-title text--semibold"><?php the_title(); ?></h1>
 			<div class="page__header-divider"></div>
@@ -68,7 +68,7 @@ $page_for_posts = get_option( 'page_for_posts' );
 	
 	elseif ( $page_for_posts ) { 
     $feat_image = 'style="background-image:url('.get_the_post_thumbnail_url( $page_for_posts ).');"';?>
-		<header class="page__header" <?php echo $feat_image; ?>>
+		<header class="page__header entry-header" <?php echo $feat_image; ?>>
 			<div class="overlay"></div>
 			<?php
 			$post = get_page($page_for_posts);
