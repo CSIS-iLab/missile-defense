f93a74ccd23ca9051d05d18275f82fd026822d50
Lindsay Urchyk <urchykli@gmail.com>
2020-11-23 15:58:45 -0500

    Featured image template part

--- wp-content/themes/missilethreat/template-parts/featured-image.php
+++ wp-content/themes/missilethreat/template-parts/featured-image.php
@@ -9,32 +9,31 @@
 
 $is_singular = is_singular();
 $is_front_page = is_front_page();
+$page_for_posts = get_option( 'page_for_posts' );
+$post_type = get_post_type();
+$img_caption = get_the_post_thumbnail_caption();
 
-if ( has_post_thumbnail() && ! post_password_required() ) {
+if ( is_singular( array( 'systems', 'actors' ) ) || is_single() && has_post_thumbnail() || is_page() ) {
+	$feat_image = get_the_post_thumbnail_url( $post->ID ); ?>
+<?php }
 
-	?>
+elseif ( is_post_type_archive() ) { 
+	$feat_image = get_archive_thumbnail_src( 'missilethreat-fullscreen' ); ?>
+<?php }
+
+elseif ( $page_for_posts ) { 
+	$feat_image = get_the_post_thumbnail_url( $page_for_posts );
+}?>
 
 	<figure class="featured-media">
 
-		<?php
-			if ( !$is_singular || $is_front_page ) {
-				echo '<a href="' . esc_url ( get_permalink() ) . '">';
-			}
+		<img src="<?php echo $feat_image ?>" alt="">
 
-			$size = '';
+		<?php if (!empty( $img_caption ) ) { ?>
 
-			if ( $is_singular && !$is_front_page ) {
-				$size = 'missilethreat-fullscreen';
-			}
+			<figcaption class="image-caption"><?php echo $img_caption; ?></figcaption>
 
-			the_post_thumbnail( $size );
-
-			if ( !$is_singular || $is_front_page ) {
-				echo '</a>';
-			}
-		?>
+			<?php } ?>
 
 	</figure><!-- .featured-media -->
 
-	<?php
-}
