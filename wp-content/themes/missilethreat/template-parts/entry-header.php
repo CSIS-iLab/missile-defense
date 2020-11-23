b817e39c45a746bdb708671cee3e52df5883c201
Lindsay Urchyk <urchykli@gmail.com>
2020-11-23 17:31:27 -0500

    Revert changes to page header images

--- wp-content/themes/missilethreat/template-parts/entry-header.php
+++ wp-content/themes/missilethreat/template-parts/entry-header.php
@@ -51,8 +51,7 @@ if( is_singular( array( 'defsys', 'missile' ) ) ) {
 
 	elseif ( is_singular( array( 'systems', 'actors' ) ) ) {
 		$feat_image = get_the_post_thumbnail_url( $post->ID ); ?>
-		<header class="page__header entry-header">
-		<?php get_template_part( 'template-parts/featured-image' ); ?>
+		<header class="page__header entry-header"  style="background-image: url('<?php echo $feat_image; ?>')">
 			<div class="page__header-inner--narrow">
 				<?php get_template_part( 'template-parts/breadcrumbs' ); ?>
 				<h1 class="page__header-title text--semibold"><?php the_title(); ?></h1>
@@ -66,8 +65,7 @@ if( is_singular( array( 'defsys', 'missile' ) ) ) {
 	
 	elseif ( is_post_type_archive() ) { 
     $feat_image = 'style="background-image:url('.get_archive_thumbnail_src( 'missilethreat-fullscreen' ).');"';?>
-		<header class="page__header entry-header">
-		<?php get_template_part( 'template-parts/featured-image' ); ?>
+		<header class="page__header entry-header" <?php echo $feat_image; ?>>
 			<h1 class="page__header-title text--semibold"><?php the_archive_title(); ?></h1>
 			<div class="page__header-divider"></div>
 			<div class="page__header-desc"><?php the_archive_top_content(); ?> </div>
@@ -84,8 +82,7 @@ if( is_singular( array( 'defsys', 'missile' ) ) ) {
 	
 	elseif ( is_page() ) { 
 		$feat_image = get_the_post_thumbnail_url( $post->ID ); ?>
-		<header class="page__header entry-header">
-		<?php get_template_part( 'template-parts/featured-image' ); ?>
+		<header class="page__header entry-header"  style="background-image: url('<?php echo $feat_image; ?>')">
 			<h1 class="page__header-title text--semibold"><?php the_title(); ?></h1>
 			<hr class="page__header-divider"></hr>
 
@@ -111,7 +108,7 @@ if( is_singular( array( 'defsys', 'missile' ) ) ) {
 					</div>
 				</div><!-- .single__header-inner -->
 				<?php
-        if ( has_post_thumbnail() ) { ?>
+				if ( has_post_thumbnail() ) { ?>
 					<div class="alignwide">
 						<div class="single__header-image-wrapper">
 							<?php get_template_part( 'template-parts/featured-image' ); ?>
@@ -124,9 +121,9 @@ if( is_singular( array( 'defsys', 'missile' ) ) ) {
 
 	<?php } 
 	
-	elseif ( $page_for_posts ) { ?>
-		<header class="page__header entry-header">
-		<?php get_template_part( 'template-parts/featured-image' ); ?>
+	elseif ( $page_for_posts ) { 
+    $feat_image = 'style="background-image:url('.get_the_post_thumbnail_url( $page_for_posts ).');"';?>
+		<header class="page__header entry-header" <?php echo $feat_image; ?>>
 			<?php
 			$post = get_page($page_for_posts);
 			setup_postdata($post); ?>
--- wp-content/themes/missilethreat/template-parts/featured-image.php
+++ wp-content/themes/missilethreat/template-parts/featured-image.php
@@ -8,24 +8,20 @@
  */
 
 
-if ( is_singular( array( 'systems', 'actors' ) ) || is_single() && has_post_thumbnail() || is_page() ) {
-	$feat_image = get_the_post_thumbnail_url( $post->ID ); ?>
-<?php }
 
-elseif ( is_post_type_archive() ) { 
-	$feat_image = get_archive_thumbnail_src( 'missilethreat-fullscreen' ); ?>
-<?php }
+if ( has_post_thumbnail() && ! post_password_required() ) {
 
-elseif ( $page_for_posts ) { 
-	$feat_image = get_the_post_thumbnail_url( $page_for_posts );
-}?>
+  ?>
 
-		<?php the_post_thumbnail(); ?>
+  <figure class="featured-media">
 
-		<figcaption class="image-caption"><?php the_post_thumbnail_caption(); ?></figcaption>
+    <?php the_post_thumbnail(); ?>
 
-	</figure><!-- .featured-media -->
+    <figcaption class="image-caption"><?php the_post_thumbnail_caption(); ?></figcaption>
+
+  </figure><!-- .featured-media -->
 
 
-	<?php
+  <?php
 }
+
