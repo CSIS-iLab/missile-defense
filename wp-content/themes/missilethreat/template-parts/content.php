87e0a51aa14e45391007d6d2d1123caaa0dbe533
Lindsay Urchyk <urchykli@gmail.com>
2020-12-08 20:06:03 -0500

    Post attribution

--- wp-content/themes/missilethreat/template-parts/content.php
+++ wp-content/themes/missilethreat/template-parts/content.php
@@ -11,6 +11,10 @@
  * @since 1.0.0
  */
 
+$object = get_queried_object();
+
+$post_attribution = get_field( 'post_attribution', $object->name );
+
 ?>
 
 <article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
@@ -30,6 +34,11 @@
 	<footer class="single__footer">
 		<?php missiledefense_share(); ?>
 		<?php echo missilethreat_authors_list_extended(); ?>
+		<p class="single__footer-attribution">
+			<?php echo $post_attribution; ?>
+		</p>
+		<?php echo missiledefense_citation(); ?>
+
 	</footer>
 
 </article><!-- .post -->
