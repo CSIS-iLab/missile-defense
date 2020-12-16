80362325e383729fee9e001c064f2d98fad1b426
Lindsay Urchyk <urchykli@gmail.com>
2020-12-16 16:30:06 -0500

    Number of results on archive page as an h2

--- wp-content/themes/missilethreat/archive.php
+++ wp-content/themes/missilethreat/archive.php
@@ -97,7 +97,7 @@ $defsys_cat_3_desc = get_field( 'category_3_description', $object->name );
       $total_posts = $wp_the_query->found_posts;
       $page = (get_query_var('paged')) ? get_query_var('paged') : 1;
       $pages = $wp_the_query->max_num_pages;
-      echo '<h3 class="archive__results">' . $total_posts . ' items, Page ' . $page . ' of ' . $pages . '</h3>';
+      echo '<h2 class="archive__results">' . $total_posts . ' items, Page ' . $page . ' of ' . $pages . '</h2>';
 
       if ( have_posts() ) {
 
