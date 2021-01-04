1aa9b18bc71ef6aac133e3c65fd2a9734cec3cf7
Lindsay Urchyk <urchykli@gmail.com>
2021-01-04 17:53:34 -0500

    Table of Contents block for resources page

--- wp-content/themes/missilethreat/blocks/lazyblock-table-of-contents/block.php
+++ wp-content/themes/missilethreat/blocks/lazyblock-table-of-contents/block.php
@@ -12,11 +12,6 @@
 
 $toc_title = get_lzb_meta('toc_title');
 $chapters = get_lzb_meta('table-of-contents');
-$appendix = get_lzb_meta('appendix');
-$download_pdf = get_lzb_meta('download-pdf');
-$report_cover = get_lzb_meta('report-cover-image');
-$graphics_link = get_lzb_meta('link-to-graphics');
-$video_link = get_lzb_meta('video');
 
 ?>
 
@@ -27,8 +22,6 @@ $video_link = get_lzb_meta('video');
 
 
   <?php 
-
-    $i = 1;
 
     if ( !$chapters ) { ?>
     <div class="tableOfContents__excerpt"><?php the_excerpt(); ?></div>
@@ -51,25 +44,10 @@ $video_link = get_lzb_meta('video');
           $chapter_title = get_the_title( $chapter_id );
         }
 
-        if( get_page_link() === $chapter_url ) {
-        ?>
-          <li><a href="<?php echo esc_url($chapter_url); ?>" target="_self" rel="nofollow" class="chapter is-active" aria-current='page'><span>Chapter <?php echo $i; ?> | </span><?php echo $chapter_title; echo missilethreat_get_svg( 'chevron-right' ); ?></a></li>
-          <?php 
-        }
-
-        else { 
           ?>
-          <li><a href="<?php echo esc_url($chapter_url); ?>" target="_self" rel="nofollow" class="chapter"><span>Chapter <?php echo $i; ?> | </span><?php echo $chapter_title; echo missilethreat_get_svg( 'chevron-right' ); ?></a></li>
+          <li><a href="<?php echo esc_url($chapter_url); ?>" target="_self" rel="nofollow" class="chapter"><?php echo $chapter_title; echo missilethreat_get_svg( 'chevron-right' ); ?></a></li>
           <?php
-        }
-        
-        $i++;
-      }
-      
-        if( isset( $appendix ) && !empty( $appendix ) ) {
-          ?>
-        <li><a href="<?php echo esc_url($appendix); ?>" target="_self" rel="nofollow" class="chapter">Appendix<?php  echo missilethreat_get_svg( 'chevron-right' ); ?></a></li>
-        <?php } ?>
+      } ?>
         
       </ul>
       
@@ -77,27 +55,6 @@ $video_link = get_lzb_meta('video');
     } ?>
 
 
-<?php
-    if( isset( $download_pdf['url'] ) && !empty( $download_pdf['url'] ) ) { ?>
-      <hr class="divider divider--blue">
-      <a href="<?php echo esc_url($download_pdf['url']); ?>" target="_blank" rel="nofollow"><button type="button" class="btn btn--short btn--dark">Download full report</button></a>
-    <?php 
-    } 
-
-    if( isset( $graphics_link ) && !empty( $graphics_link ) ) { ?>
-    <li class="tableOfContents__graphics"><a href="<?php echo esc_url($graphics_link); ?>" target="_self" rel="nofollow">See all illustrations & graphics<?php  echo missilethreat_get_svg( 'chevron-right' ); ?></a></li>
-    <?php
-    } 
-
-    if( isset( $video_link ) && !empty( $video_link ) ) { ?>
-    <li class="tableOfContents__video-link"><a href="<?php echo esc_url($video_link); ?>" target="_blank" rel="nofollow">Watch the video<?php  echo missilethreat_get_svg( 'external-link' ); ?></a></li>
-    <?php } ?>
-
   </ul>
   </div>
-<?php
-  if ( isset( $report_cover['link'] ) && !empty( $report_cover['link'] ) ) { ?>
-    <img src="<?php echo esc_url($report_cover['link']); ?>" alt="<?php echo $report_cover['alt']; ?>">
-<?php } ?>
-
 </div>
