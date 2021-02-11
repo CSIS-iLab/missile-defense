6710f17bf7dea4c52affc5bad7e4bdf8804bca17
Lindsay Urchyk <urchykli@gmail.com>
2021-02-11 16:07:48 -0500

    Update homepage hero initiative text

--- wp-content/themes/missilethreat/front-page.php
+++ wp-content/themes/missilethreat/front-page.php
@@ -19,7 +19,7 @@ get_header();
 			<?php the_content(); ?>
 		</div>
 		<div class="home__credits">
-			<p class="home__initiative">an initiative from<br/><a href="https://www.csis.org/programs/international-security-program/missile-defense-project" class="text--semibold">Missile Defense Project</a></p>
+			<p class="home__initiative">an initiative of the CSIS<br/><a href="https://www.csis.org/programs/international-security-program/missile-defense-project" class="text--semibold">Missile Defense Project</a></p>
 			<a href="https://www.csis.org" class="home__logo"><?php include( get_template_directory() . '/assets/static/csis-logo.svg'); ?></a>
 		</div>
 	</div>
